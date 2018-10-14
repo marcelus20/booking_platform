e5ad81f75299c7f9afb4a67d9acd37a418bbd99a
felipe <marcelus20felipe@gmail.com>
2018-10-14 21:22:17 +0100

    redesigned all database tables

--- databases/db.sql
+++ databases/db.sql
@@ -1,44 +1,112 @@
-
-CREATE TABLE users (
-                user_id INT AUTO_INCREMENT NOT NULL,
-                user_type VARCHAR(1) NOT NULL,
+DROP TABLE IF EXISTS admin;
+CREATE TABLE admin (
+                id INT AUTO_INCREMENT NOT NULL,
+                password VARCHAR(128) NOT NULL,
                 email VARCHAR(60) NOT NULL,
-                user_last_name VARCHAR(25),
-                user_password VARCHAR(500),
-                date_joined DATE NOT NULL,
-                user_phone VARCHAR(30),
-                user_first_name VARCHAR(25),
-                PRIMARY KEY (user_id, user_type)
+                PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS service_provider;
+CREATE TABLE service_provider (
+                s_id INT AUTO_INCREMENT NOT NULL,
+                password VARCHAR(128) NOT NULL,
+                email VARCHAR(60) NOT NULL,
+                date_of_account_creation DATE NOT NULL,
+                company_full_name VARCHAR(40) NOT NULL,
+                PRIMARY KEY (s_id)
 );
 
 
+CREATE UNIQUE INDEX service_provider_idx
+ ON service_provider
+ ( email );
+
+
+DROP TABLE IF EXISTS location;
 CREATE TABLE location (
-                user_id INT NOT NULL,
-                user_type VARCHAR(1) NOT NULL,
-                address_line_2 VARCHAR(70),
-                city VARCHAR(50) DEFAULT Dublin NOT NULL,
-                address_line_1 VARCHAR(70) NOT NULL,
-                PRIMARY KEY (user_id, user_type)
+                s_id INT NOT NULL,
+                first_line_address VARCHAR(50) NOT NULL,
+                eir_code VARCHAR(10) NOT NULL,
+                city VARCHAR(20) DEFAULT 'Dublin' NOT NULL,
+                second_line_address VARCHAR(30),
+                PRIMARY KEY (s_id)
+);
+
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers (
+                customer_id INT AUTO_INCREMENT NOT NULL,
+                password VARCHAR(128) NOT NULL,
+                email VARCHAR(60) NOT NULL,
+                phone VARCHAR(30) NOT NULL,
+                first_name VARCHAR(25) NOT NULL,
+                last_name VARCHAR(25) NOT NULL,
+                date_of_account_creation DATE NOT NULL,
+                PRIMARY KEY (customer_id)
 );
 
 
-CREATE TABLE bookings (
-                user_id INT NOT NULL,
-                user_type VARCHAR(1) NOT NULL,
-                status VARCHAR(10) NOT NULL,
-                b_time TIME NOT NULL,
-                PRIMARY KEY (user_id, user_type)
+CREATE UNIQUE INDEX customers_idx
+ ON customers
+ ( email, phone );
+
+DROP TABLE IF EXISTS complaints;
+CREATE TABLE complaints (
+                customer_id INT NOT NULL,
+                s_id INT NOT NULL,
+                complaint VARCHAR(500) NOT NULL,
+                PRIMARY KEY (customer_id, s_id)
+);
+
+DROP TABLE IF EXISTS booking;
+CREATE TABLE booking (
+                customer_id INT NOT NULL,
+                s_id INT NOT NULL,
+                time TIME NOT NULL,
+                PRIMARY KEY (customer_id, s_id)
 );
 
 
-ALTER TABLE location ADD CONSTRAINT users_location_fk
-FOREIGN KEY (user_id, user_type)
-REFERENCES users (user_id, user_type)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+DROP TABLE IF EXISTS service_type;
+CREATE TABLE service_type (
+                customer_id INT NOT NULL,
+                s_id INT NOT NULL,
+                type VARCHAR(20) NOT NULL,
+                PRIMARY KEY (customer_id, s_id)
+);
 
-ALTER TABLE bookings ADD CONSTRAINT location_bookings_fk
-FOREIGN KEY (user_type, user_id)
-REFERENCES location (user_type, user_id)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+
+ALTER TABLE location ADD CONSTRAINT service_provider_location_fk
+FOREIGN KEY (s_id)
+REFERENCES service_provider (s_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE booking ADD CONSTRAINT service_provider_booking_fk
+FOREIGN KEY (s_id)
+REFERENCES service_provider (s_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE complaints ADD CONSTRAINT service_provider_complaints_fk
+FOREIGN KEY (s_id)
+REFERENCES service_provider (s_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE booking ADD CONSTRAINT customers_booking_fk
+FOREIGN KEY (customer_id)
+REFERENCES customers (customer_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE complaints ADD CONSTRAINT customers_complaints_fk
+FOREIGN KEY (customer_id)
+REFERENCES customers (customer_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE service_type ADD CONSTRAINT booking_service_type_fk
+FOREIGN KEY (customer_id, s_id)
+REFERENCES booking (customer_id, s_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
