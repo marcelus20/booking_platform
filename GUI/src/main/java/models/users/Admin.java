7200c46d9b11ddf8827a3785898d6428a0dc2991
felipe <marcelus20felipe@gmail.com>
2018-10-18 23:41:12 +0100

    made Admin extend abstract user

--- GUI/src/main/java/models/users/Admin.java
+++ GUI/src/main/java/models/users/Admin.java
@@ -2,70 +2,24 @@ package models.users;
 
 import java.util.Objects;
 
-public class Admin {
+public class Admin extends AbstractUser{
 
-    private Long id;
-    private String password;
-    private String email;
 
-    public Admin(Long id, String password, String email) {
-        this.id = id;
-        this.password = password;
-        this.email = email;
-    }
-
-    public Admin() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Admin withId(Long id) {
+    @Override
+    public AbstractUser withId(Long id) {
         this.id = id;
         return this;
     }
 
-    public Admin withPassword(String password) {
+    @Override
+    public AbstractUser withEmail(String email) {
+        this.eMail = email;
+        return this;
+    }
+
+    @Override
+    public AbstractUser withPassword(String password) {
         this.password = password;
         return this;
-    }
-
-    public Admin withEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Admin admin = (Admin) o;
-        return Objects.equals(id, admin.id) &&
-                Objects.equals(password, admin.password) &&
-                Objects.equals(email, admin.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, password, email);
-    }
-
-    @Override
-    public String toString() {
-        return "Admin{" +
-                "id=" + id +
-                ", password='" + password + '\'' +
-                ", eMail='" + email + '\'' +
-                '}';
     }
 }
