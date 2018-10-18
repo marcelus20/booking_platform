3bd88f2255d5de02b0cdcf3958af0d3440f390da
felipe <marcelus20felipe@gmail.com>
2018-10-18 23:44:54 +0100

    removed the database connector from Main

--- GUI/src/main/java/main/Main.java
+++ GUI/src/main/java/main/Main.java
@@ -1,43 +1,20 @@
 package main;
 
-import models.users.Customer;
-
-import java.sql.*;
+import controllers.LoginController;
+import interfaces.Controlls;
 
 
 public class Main {
 
     private Main(){
-        Customer cs = new Customer();
-        cs.withId((long)1).withEmail("blah@gmail.com").withPassword("12345");
-        cs.withFirstName("Felipe").withLastName("Mantovani");
-        System.out.println(cs);
+        new LoginController();
 
     }
 
 
     public static void main(String... args){
         new Main();
-        /*
-        try{
-            Connection myConn = DriverManager
-                    .getConnection("jdbc:mysql://localhost/booking_platform", "root", "");
 
-
-            Statement myStmt = myConn.createStatement();
-
-            String query = new StringBuilder().append("SELECT * FROM admin").toString();
-
-            ResultSet rs = myStmt.executeQuery(query);
-
-            while (rs.next()){
-                System.out.println(rs.getString("password"));
-                System.out.println(rs.getString("eMail"));
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        */
 
     }
 }
