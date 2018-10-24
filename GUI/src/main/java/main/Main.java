3656700a3471d00cf2e7dbdcc241606b461fd750
felipe <marcelus20felipe@gmail.com>
2018-10-24 18:20:39 +0100

    Form test and login included in Main

--- GUI/src/main/java/main/Main.java
+++ GUI/src/main/java/main/Main.java
@@ -1,18 +1,40 @@
 package main;
 
-import controllers.loginControllers.LoginController;
+import controllers.signUpFormControllers.AdminSignUpFormController;
 
 
-public class Main {
+import javax.swing.*;
+import java.sql.SQLException;
 
-    private Main(){
-        new LoginController();
+
+public class Main extends JFrame {
+
+    private Main() throws SQLException {
+
+
+
+        //this.add(ServiceProviderSignUpFormController.initServiceProviderSignUpFormController().getViewObject());
+        //this.add(CustomerSignUpFormController.initCustomerSignUpController().getViewObject());
+        this.add(AdminSignUpFormController.initAdminSignUpController().getViewObject());
+        this.setVisible(true);
+        this.setSize(550,500);
+        this.repaint();
+        this.validate();
+
 
     }
 
+    Main(int a){
+        long millis=System.currentTimeMillis();
+        java.sql.Date date=new java.sql.Date(millis);
+        System.out.println(date);
+    }
 
-    public static void main(String... args){
+    public static void main(String... args) throws SQLException {
         new Main();
+        //new Main(1);
+
+        //new LoginController();
 
 
     }
