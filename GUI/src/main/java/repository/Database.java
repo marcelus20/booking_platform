cfe53b1d1d5338358bab3e937a89bcc6ea5aa22c
felipe <marcelus20felipe@gmail.com>
2018-10-24 18:10:12 +0100

    Created the abstract class Database that will have the connection with database and the statement attributes initialised.

---
+++ GUI/src/main/java/repository/Database.java
@@ -0,0 +1,41 @@
+package repository;
+
+import interfaces.Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public abstract class Database implements Repository {
+
+    protected static Connection myConn;
+    protected static Statement myStmt;
+
+    public Database() throws SQLException {
+         myConn = DriverManager
+                .getConnection("jdbc:mysql://localhost/booking_platform", "root", "");
+         myStmt = myConn.createStatement();
+    }
+
+    @Override
+    public void insertData(Object obj) {
+
+    }
+
+    @Override
+    public List<List> selectData(String query) {
+        return null;
+    }
+
+    @Override
+    public Object login(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public void updateData() {
+
+    }
+}
