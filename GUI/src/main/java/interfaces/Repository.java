cf7fa914fe3b9ddc07db4c8103c5b4e9930bc350
felipe <marcelus20felipe@gmail.com>
2018-10-26 08:58:10 +0100

    Taken out one of the two Generics diamond class signature and declared one more method that returns the id

--- GUI/src/main/java/interfaces/Repository.java
+++ GUI/src/main/java/interfaces/Repository.java
@@ -1,17 +1,26 @@
 package interfaces;
 
+import models.users.AbstractUser;
+
 import java.sql.ResultSet;
 import java.util.List;
 
-public interface Repository <T, G> {
+public interface Repository <T> {
 
     void insertData(Object obj);
 
     List<List> selectData(String query);
 
-    G login(String email, String password);
+    AbstractUser login(String email, String password);
 
     void updateData();
+
+    /**
+     * this method will take the User as a parameter, look for this user in the database and return its id.
+     * @param user
+     * @return
+     */
+    Long getId(AbstractUser user);
 
 
 }
