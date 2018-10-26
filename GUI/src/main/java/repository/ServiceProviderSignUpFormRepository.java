53fd933a0e42373e9f879d722fdb785188caf114
felipe <marcelus20felipe@gmail.com>
2018-10-26 08:57:08 +0100

    Location is now being inserted to the table location in the database linked with the service_provider id

--- GUI/src/main/java/repository/ServiceProviderSignUpFormRepository.java
+++ GUI/src/main/java/repository/ServiceProviderSignUpFormRepository.java
@@ -12,7 +12,7 @@ import java.sql.*;
 import java.util.List;
 import java.sql.Date;
 
-public class ServiceProviderSignUpFormRepository extends Database{
+public class ServiceProviderSignUpFormRepository extends Database {
 
 
     public ServiceProviderSignUpFormRepository() throws SQLException {
@@ -26,7 +26,7 @@ public class ServiceProviderSignUpFormRepository extends Database{
         try{
             //Here is where it will look in all tables to see if it finds a user
 
-            String queryInsertionToServiceProviderTable = new StringBuilder()
+            String query = new StringBuilder()
                     .append("INSERT INTO service_provider ")
                     .append("(phone, password, email, date_of_account_creation, company_full_name, approved_status) ")
                     .append("VALUES( ")
@@ -44,10 +44,47 @@ public class ServiceProviderSignUpFormRepository extends Database{
                     .append(");")
                     .toString();
 
-            System.out.println(queryInsertionToServiceProviderTable);
+            System.out.println(query);
+
+            myStmt.executeUpdate(query);
+
+            query = new StringBuilder().append("SELECT * FROM service_provider ")
+                    .append("WHERE ")
+                    .append("company_full_name = ")
+                    .append("'").append(user.getCompanyFullName()).append("'")
+                    .append(" AND ")
+                    .append(" email = ")
+                    .append("'").append(user.geteMail()).append("'")
+                    .append(";").toString();
+            System.out.println(query);
+
+            Long id = 0L;
+
+            ResultSet rs = myStmt.executeQuery(query);
+            while (rs.next()){
+                id = Long.parseLong(rs.getString("s_id"));
+            }
+
+            query = new StringBuilder().append("INSERT INTO location ")
+                    .append("(s_id, first_line_address, eir_code, city, second_line_address) ")
+                    .append(" VALUES (")
+                    .append("'").append(id).append("'")
+                    .append(", ")
+                    .append("'").append(location.getFirst_line_address()).append("'")
+                    .append(", ")
+                    .append("'").append(location.getEir_code()).append("'")
+                    .append(", ")
+                    .append("'").append(location.getCity()).append("'")
+                    .append(", ")
+                    .append("'").append(location.getSecond_line_address()).append("'")
+                    .append(");")
+                    .toString();
+
+            System.out.println(query);
+
+            myStmt.executeUpdate(query);
 
 
-            myStmt.executeUpdate(queryInsertionToServiceProviderTable);
 
             } catch (SQLException e1) {
             e1.printStackTrace();
@@ -64,7 +101,7 @@ public class ServiceProviderSignUpFormRepository extends Database{
     }
 
     @Override
-    public Object login(String email, String password) {
+    public AbstractUser login(String email, String password) {
         return null;
     }
 
@@ -72,4 +109,12 @@ public class ServiceProviderSignUpFormRepository extends Database{
     public void updateData() {
 
     }
+
+    @Override
+    public Long getId(AbstractUser user) {
+        user = (ServiceProvider) user;
+        return user.getId();
+    }
+
+
 }
