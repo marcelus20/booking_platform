c4ff2f9e5479af64f6a7bcd3007f3ce9c0f46421
felipe <marcelus20felipe@gmail.com>
2018-10-29 17:26:57 +0000

    The class has been renamed to Forms and methods were added to it, it has the two public Forms (Service and Customer forms).

---
+++ GUI/src/main/java/views/signUpForms/Forms.java
@@ -0,0 +1,55 @@
+package views.signUpForms;
+
+import static controllers.signUpFormControllers
+        .CustomerSignUpFormController.initCustomerSignUpController;
+import static controllers.signUpFormControllers
+        .ServiceProviderSignUpFormController.initServiceProviderSignUpFormController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLException;
+
+
+public class Forms extends JFrame {
+
+    private CustomerSignUpForm customerSignUpForm;
+    private ServiceProviderSignUpForm serviceProviderSignUpForm;
+    private JPanel sideBar;
+    private JPanel formView;
+    private JButton toggleCustomerButton;
+    private JButton toggleServiceProviderButton;
+
+
+    public Forms() throws HeadlessException, SQLException {
+        customerSignUpForm = initCustomerSignUpController().getViewObject();
+        serviceProviderSignUpForm = initServiceProviderSignUpFormController().getViewObject();
+        sideBar = new JPanel();
+        formView = new JPanel();
+        toggleCustomerButton = new JButton("Sign up as a Costumer");
+        toggleServiceProviderButton = new JButton("Sign up as a Service Provider");
+    }
+
+    public CustomerSignUpForm getCustomerSignUpForm() {
+        return customerSignUpForm;
+    }
+
+    public ServiceProviderSignUpForm getServiceProviderSignUpForm() {
+        return serviceProviderSignUpForm;
+    }
+
+    public JPanel getSideBar() {
+        return sideBar;
+    }
+
+    public JPanel getFormView() {
+        return formView;
+    }
+
+    public JButton getToggleCustomerButton() {
+        return toggleCustomerButton;
+    }
+
+    public JButton getToggleServiceProviderButton() {
+        return toggleServiceProviderButton;
+    }
+}
