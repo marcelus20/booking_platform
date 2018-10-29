ce8e326e148dc2ab2fa2818d0fea12e64f94a8fe
felipe <marcelus20felipe@gmail.com>
2018-10-29 17:29:42 +0000

    intanciated Application controller class to the static void main method. All the others scratching comments and tested methods were removed

--- GUI/src/main/java/main/Main.java
+++ GUI/src/main/java/main/Main.java
@@ -1,8 +1,6 @@
 package main;
 
-import controllers.signUpFormControllers.AdminSignUpFormController;
-import controllers.signUpFormControllers.ServiceProviderSignUpFormController;
-
+import controllers.Application;
 
 import javax.swing.*;
 import java.sql.SQLException;
@@ -10,32 +8,10 @@ import java.sql.SQLException;
 
 public class Main extends JFrame {
 
-    private Main() throws SQLException {
-
-
-
-        this.add(ServiceProviderSignUpFormController.initServiceProviderSignUpFormController().getViewObject());
-        //this.add(CustomerSignUpFormController.initCustomerSignUpController().getViewObject());
-        //this.add(AdminSignUpFormController.initAdminSignUpController().getViewObject());
-        this.setVisible(true);
-        this.setSize(550,500);
-        this.repaint();
-        this.validate();
-
-
-    }
-
-    Main(int a){
-        long millis=System.currentTimeMillis();
-        java.sql.Date date=new java.sql.Date(millis);
-        System.out.println(date);
-    }
 
     public static void main(String... args) throws SQLException {
-        new Main();
-        //new Main(1);
 
-        //new LoginController();
+        new Application();
 
 
     }
