0224c949c5263df328d5cd30f27ca612a3cf3f66
felipe <marcelus20felipe@gmail.com>
2018-10-31 23:10:37 +0000

    Generified the List of MappedResults type. Turned attributes into protected and created TableOfBookings, Service Provider and Customer to extend this TableOfEntitiesController with the proper generics types.

---
+++ GUI/src/main/java/controllers/dashboardController/adminControlls/TablesOfCustomers.java
@@ -0,0 +1,28 @@
+package controllers.dashboardController.adminControlls;
+
+import models.users.Customer;
+import repository.CustomerRepository;
+import utils.Tools;
+import views.dashboard.adminView.ListOfCustomers;
+import javax.swing.*;
+import java.sql.SQLException;
+
+public class TablesOfCustomers
+        extends TableOfEntitiesController<ListOfCustomers, CustomerRepository, Customer> {
+    public TablesOfCustomers(ListOfCustomers tableOfEntities, CustomerRepository repository) throws SQLException {
+        super(tableOfEntities, repository);
+        config();
+        build();
+        setSizes();
+    }
+
+    @Override
+    public void build() {
+        super.build();
+        String[] columns = new String[]{"id", "email",  "phone", "first name", "last name","date_of_account_creation"};
+        tableOfEntities.setTable(new JTable(Tools.convertListToArray(results), columns));
+        tableOfEntities.setScrollPane(new JScrollPane(tableOfEntities.getTable()));
+        tableOfEntities.add(tableOfEntities.getScrollPane());
+    }
+
+}
