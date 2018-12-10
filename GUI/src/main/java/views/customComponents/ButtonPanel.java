f87a1cf78defd2b4c9d9de0ab137767f14728a7f
felipe <marcelus20felipe@gmail.com>
2018-12-10 14:25:56 +0000

    commented ButtonPanel

--- GUI/src/main/java/views/customComponents/ButtonPanel.java
+++ GUI/src/main/java/views/customComponents/ButtonPanel.java
@@ -3,17 +3,27 @@ package views.customComponents;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * This class class is a Panel with a button inside.
+ * Used to create JButtons with a pre configured class.
+ *
+ */
 public class ButtonPanel extends JPanel {
 
+    /**
+     * The JButton attribute
+     */
     private final MyCustomJButton button;
 
+    /**
+     * Constructor
+     * @param buttonLabel
+     */
     public ButtonPanel(String buttonLabel) {
-
-        this.button = new MyCustomJButton(buttonLabel);
-        setBorder(BorderFactory.createLineBorder(Color.lightGray));
+        this.button = new MyCustomJButton(buttonLabel);//setting up button
+        setBorder(BorderFactory.createLineBorder(Color.lightGray)); //creating border
         Integer cols = 2;
-
-        setLayout(new GridLayout(1,cols));
+        setLayout(new GridLayout(1,cols)); // seting to grid layout manager
         add(button.getButton());
     }
 
