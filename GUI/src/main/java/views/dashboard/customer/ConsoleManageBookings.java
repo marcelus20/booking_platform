933d0c2002ea910f87aedcf38a255f219d394b55
felipe <marcelus20felipe@gmail.com>
2018-12-10 15:35:19 +0000

    commented the manageBookings class

--- GUI/src/main/java/views/dashboard/customer/ConsoleManageBookings.java
+++ GUI/src/main/java/views/dashboard/customer/ConsoleManageBookings.java
@@ -2,27 +2,25 @@ package views.dashboard.customer;
 
 import views.customComponents.MyCustomJLabel;
 import views.customComponents.MyCustomJPanel;
-
 import javax.swing.*;
-import java.awt.*;
 
-public class ConsoleManageBookings extends MyCustomJPanel {
+/**
+ * This class is to visually list all bookings that the logged user has booked.
+ */
+public class ConsoleManageBookings extends MyCustomJPanel { // it is a improved JPanel
 
+    /**
+     * List of attributes: a Jtable and scroll pane for the data
+     */
     private JTable tableOfMyBookings;
     private JScrollPane jScrollPane;
 
-//    public ConsoleManageBookings(String[][] table, String[] colNames) {
-//        super("Manage your bookings", 700, 500);
-//
-//        tableOfMyBookings = new JTable(table, colNames);
-//
-//
-//
-//
-//        tableOfMyBookings.setPreferredScrollableViewportSize(new Dimension(680,500));
-//        getContent().add(jScrollPane);
-//    }
 
+    /**
+     * Constructor: controller will be retrieving data from DB and populating this jTable with
+     * data.
+     * @param jTable
+     */
     public ConsoleManageBookings(JTable jTable) {
         super("Manage your bookings", 700, 500);
         tableOfMyBookings = jTable;
