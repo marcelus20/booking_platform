973ef967fb0b211b13496f4ece27932e3ed18a58
felipe <marcelus20felipe@gmail.com>
2018-12-10 09:04:35 +0000

    commented AdminRepository

--- GUI/src/main/java/models/repositories/AdminRepository.java
+++ GUI/src/main/java/models/repositories/AdminRepository.java
@@ -4,20 +4,27 @@ import models.Database;
 import models.enums.UserType;
 import models.tuples.entitiesRepresentation.AbstraticUser;
 import models.tuples.entitiesRepresentation.Admin;
-
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * This class is an implementation of the Repository interface.
+ * It tuned out that for the porpuses of the project, the only function implemented was the addToDB
+ * when an admin is registering another admin
+ */
 public class AdminRepository implements Repository<Admin> {
     public AdminRepository() {
     }
 
     @Override
     public void addToDB(Admin obj) {
-        Admin admin = (Admin)obj;
+        Admin admin = obj; // declaring admin object
         try {
-            Database.database().getStmt().executeUpdate(
+            /**
+             * creatingg an statement from Database Singleton.
+             */
+            Database.database().getStmt().executeUpdate(//executing query.
                     "INSERT INTO users (email, password, user_type, date_created)" +
                             " VALUES ('"+admin.getEmail()
                     +"', '"+admin.getPassword()+"', '"+ UserType.ADMIN +"', '"+new Date(System.currentTimeMillis()) +"')");
