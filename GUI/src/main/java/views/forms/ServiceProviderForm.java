e52811a0fe722888c88014946de3b203875b1677
felipe <marcelus20felipe@gmail.com>
2018-12-10 16:18:38 +0000

    commented ServiceProviderForm

--- GUI/src/main/java/views/forms/ServiceProviderForm.java
+++ GUI/src/main/java/views/forms/ServiceProviderForm.java
@@ -7,14 +7,24 @@ import views.customComponents.InputPanel;
 import javax.swing.*;
 import java.util.List;
 
-public class ServiceProviderForm extends SignUp implements InputPanelContainer {
+/**
+ * the service provider provider forms extends Signup  class and have few extra attributes for the location wise
+ * Signup class implements InputPanelContainer so its method has been implemented in the botton of the class
+ */
+public class ServiceProviderForm extends SignUp{
 
+    /**
+     * List of attributes
+     */
     private final InputPanel companyFullName;
     private final InputPanel firstLineAddress;
     private final InputPanel secondLineAddress;
     private final InputPanel city;
     private final InputPanel eircode;
 
+    /**
+     * CONSTRUCTOR
+     */
     public ServiceProviderForm() {
         super("Service Provider Sign Up");
         companyFullName = new InputPanel("company full name: ", new JTextField());
@@ -22,7 +32,6 @@ public class ServiceProviderForm extends SignUp implements InputPanelContainer {
         secondLineAddress = new InputPanel("Second Line Address: ", new JTextField());
         city = new InputPanel("city: ", new JTextField());
         eircode = new InputPanel("Eir Code: ", new JTextField());
-
         add(companyFullName);
         add(firstLineAddress);
         add(secondLineAddress);
@@ -31,6 +40,10 @@ public class ServiceProviderForm extends SignUp implements InputPanelContainer {
         addFormButtons();
     }
 
+    /**
+     * getters
+     * @return
+     */
     public String getCompanyFullName() {
         return companyFullName.getInput().getInput().getText().trim();
     }
@@ -51,9 +64,13 @@ public class ServiceProviderForm extends SignUp implements InputPanelContainer {
         return eircode.getInput().getInput().getText().trim();
     }
 
+    /**
+     * InputPanelController override method.
+     * @return
+     */
     @Override
     public List<InputPanel> getInputsPanel() {
-        return arrayListBuilder(super.getInputsPanel())
+        return arrayListBuilder(super.getInputsPanel()) // calling super class method and joining with this class
                 .add(companyFullName).add(firstLineAddress).add(secondLineAddress)
                 .add(city).add(eircode).build();
     }
