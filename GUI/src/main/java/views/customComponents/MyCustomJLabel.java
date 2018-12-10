ab82f4e63c945e04442b678df0a97c78b00f272c
felipe <marcelus20felipe@gmail.com>
2018-12-10 14:51:03 +0000

    commented MyCustomJLabel

--- GUI/src/main/java/views/customComponents/MyCustomJLabel.java
+++ GUI/src/main/java/views/customComponents/MyCustomJLabel.java
@@ -14,33 +14,62 @@ import java.awt.*;
 
 public class MyCustomJLabel{
 
+    /**
+     * Attribute
+     */
     private final JLabel label;
 
+    /**
+     * CONSTRUCTOR 1
+     * @param label
+     * @param font
+     */
     public MyCustomJLabel(final String label, final Font font) {
-
         this.label = new JLabel(Tools.wrapStringWithHtml(label), SwingConstants.CENTER);
         this.label.setFont(font);
     }
 
+    /**
+     * CONSTRUCTOR 2
+     * @param label
+     * @param fontType
+     * @param fontSize
+     */
     public MyCustomJLabel(final String label, final String fontType, final Integer fontSize) {
         this.label = new JLabel(Tools.wrapStringWithHtml(label), SwingConstants.CENTER);
         this.label.setFont(new Font(fontType, Font.PLAIN, fontSize));
     }
 
+    /**
+     * CONSTRUCTOR 3
+     * @param label
+     */
     public MyCustomJLabel(final String label) {
         this.label = new JLabel(Tools.wrapStringWithHtml(label), SwingConstants.CENTER);
         this.label.setFont(new MyCustomFont().getFont());
     }
 
+    /**
+     * CONSTRUCTOR 4
+     * @param label
+     * @param fontSize
+     */
     public MyCustomJLabel(final String label, final Integer fontSize) {
         this.label = new JLabel(Tools.wrapStringWithHtml(label), SwingConstants.CENTER);
         this.label.setFont(new MyCustomFont(fontSize).getFont());
     }
 
+    /**
+     * GETTER FOR THE LABEL
+     * @return
+     */
     public JLabel getLabel() {
         return label;
     }
 
+    /**
+     * Sets font to white color
+     */
     public void setFontToWhite(){
         this.label.setForeground(Color.WHITE);
     }
