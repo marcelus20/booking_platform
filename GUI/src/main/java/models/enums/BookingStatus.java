e306a27d538277812c4221745429551405d98c72
felipe <marcelus20felipe@gmail.com>
2018-12-10 08:45:17 +0000

    commented BookingStatus

--- GUI/src/main/java/models/enums/BookingStatus.java
+++ GUI/src/main/java/models/enums/BookingStatus.java
@@ -1,5 +1,9 @@
 package models.enums;
 
+/**
+ * This enum list the states of a bookingStatus object. I can either be CONFIRMED, PENDENT, COMPLETE OR
+ * USER_DID_NOT_ARRIVE. This is to be used by service providers when setting up a status of a booking.
+ */
 public enum BookingStatus {
 
     CONFIRMED,
