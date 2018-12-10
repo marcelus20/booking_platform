9f9a4546ecd73f7116edc145966aca3a7c966a3c
felipe <marcelus20felipe@gmail.com>
2018-12-10 11:39:13 +0000

    Commented Log

--- GUI/src/main/java/models/tuples/entitiesRepresentation/Log.java
+++ GUI/src/main/java/models/tuples/entitiesRepresentation/Log.java
@@ -2,15 +2,30 @@ package models.tuples.entitiesRepresentation;
 
 import java.util.Objects;
 
+/**
+ * Entity representation of the Log table in the database.
+ */
 public class Log {
+    /**
+     * LIST OF ATTRIBUTE - COLUMNS
+     */
     private String userId;
     private String activityLog;
 
+    /**
+     * FULL CONSTRUCTOR
+     * @param userId COLUMN
+     * @param activityLog COLUMN
+     */
     public Log(String userId, String activityLog) {
         this.userId = userId;
         this.activityLog = activityLog;
     }
 
+    /**
+     * GETTERS AND SETTERS
+     * @return
+     */
     public String getUserId() {
         return userId;
     }
@@ -27,6 +42,11 @@ public class Log {
         this.activityLog = activityLog;
     }
 
+    /**
+     * EQUALS
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -36,11 +56,19 @@ public class Log {
                 Objects.equals(activityLog, log.activityLog);
     }
 
+    /**
+     * HASHCODE
+     * @return
+     */
     @Override
     public int hashCode() {
         return Objects.hash(userId, activityLog);
     }
 
+    /**
+     * TOSTRING
+     * @return
+     */
     @Override
     public String toString() {
         return "Log{" +
