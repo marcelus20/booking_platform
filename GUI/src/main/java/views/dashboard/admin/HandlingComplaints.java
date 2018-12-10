5b8522f0fd05ff1505e71f01799e34ab209e602b
felipe <marcelus20felipe@gmail.com>
2018-12-10 15:14:41 +0000

    comented HandlingComplaints

--- GUI/src/main/java/views/dashboard/admin/HandlingComplaints.java
+++ GUI/src/main/java/views/dashboard/admin/HandlingComplaints.java
@@ -3,10 +3,22 @@ import views.customComponents.MyCustomJPanel;
 
 import javax.swing.*;
 
+/**
+ * The handling complaints Panel.
+ * this panel will be toggled when admin clicks on the handle Complaints button in dashboard sidebar panel.
+ */
 public class HandlingComplaints extends MyCustomJPanel {
 
+    /**
+     * contains a table of complaints
+     */
     private JTable complaintTable;
 
+    /**
+     * Constructor:
+     * the table of complaints come from outside, controller will be passing a JTable with data from db
+     * @param complaintTable
+     */
     public HandlingComplaints(JTable complaintTable) {
         super("Complaints Handler Panel - click on the line to change complaint status", 500,500);
         this.complaintTable = complaintTable;
