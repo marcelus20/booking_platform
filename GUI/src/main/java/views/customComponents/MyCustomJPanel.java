86c8ef27fd4458d91a9f56c4e077cd59c43da29a
felipe <marcelus20felipe@gmail.com>
2018-12-10 14:55:08 +0000

    commented MyCustomJPanel

--- GUI/src/main/java/views/customComponents/MyCustomJPanel.java
+++ GUI/src/main/java/views/customComponents/MyCustomJPanel.java
@@ -4,15 +4,33 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
+/**
+ * This is my custom JPanel class. very well used throughout the the project.
+ * The special thing about this class is that it is a JPanel that comes with a title,
+ * border, and a panel with the content to be filled by the controllers.
+ *
+ * You just need to use one of the constructors and then everything else will be configured
+ * in a nice layout, title and content.
+ */
 public class MyCustomJPanel extends JPanel{
 
+    /**
+     * List of attributes
+     */
     private final MyCustomJLabel title;
     private final Border border;
     private final Dimension size;
     private final JPanel header;
-    private JPanel content;
+    private JPanel content; // -> this guy is the only one that controllers will fill with content
 
+    /**
+     * Constructor
+     * @param title
+     * @param width
+     * @param height
+     */
     public MyCustomJPanel(String title, Integer width, Integer height) {
+        //CONFIGURING ALL GUI COMPONENTS
         this.title = new MyCustomJLabel(title); this.title.setFontToWhite();
         this.border = BorderFactory.createLineBorder(Color.LIGHT_GRAY);
         this.size = new Dimension(width, height);
@@ -26,6 +44,10 @@ public class MyCustomJPanel extends JPanel{
         setPreferredSize(size); setBorder(border);
     }
 
+    /**
+     * GETTERS AND SETTERS
+     * @return
+     */
     public JPanel getContent() {
         return content;
     }
