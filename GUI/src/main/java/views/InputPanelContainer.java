649b9b961b7d5ee257fe6912582d9c59df8e799c
felipe <marcelus20felipe@gmail.com>
2018-12-10 16:33:28 +0000

    commented InpuPanelContainer interface

--- GUI/src/main/java/views/InputPanelContainer.java
+++ GUI/src/main/java/views/InputPanelContainer.java
@@ -1,10 +1,17 @@
 package views;
 
 import views.customComponents.InputPanel;
-
 import java.util.List;
 
+/**
+ * This interface is to be implemented by classes that holds many inputs panels such as Forms,
+ * Signup registration forms.
+ */
 public interface InputPanelContainer {
 
+    /**
+     * returns an array of all inputspanels that the Frame/Panel contains.
+     * @return
+     */
     List<InputPanel> getInputsPanel();
 }
