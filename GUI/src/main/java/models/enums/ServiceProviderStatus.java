b47d204fd3a6ce53f1ccb3d97fabcf0356a7bfff
felipe <marcelus20felipe@gmail.com>
2018-12-10 08:49:26 +0000

    commented ServiceProviderStatus

--- GUI/src/main/java/models/enums/ServiceProviderStatus.java
+++ GUI/src/main/java/models/enums/ServiceProviderStatus.java
@@ -1,7 +1,9 @@
 package models.enums;
 
+/**
+ * This enum is to be used by a service provider object that is either pendent, approved or reject.
+ * Admins will handle this enum to set status for the new registered service provider.
+ */
 public enum ServiceProviderStatus {
     PENDENT, APPROVED, REJECTED;
-
-
 }
