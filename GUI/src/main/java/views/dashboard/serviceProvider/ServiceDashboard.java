2c6ccf5a09d1afc0184dc84f8e2a20878aa19290
felipe <marcelus20felipe@gmail.com>
2018-12-10 15:51:52 +0000

    commented ServiceDashboardClass

--- GUI/src/main/java/views/dashboard/serviceProvider/ServiceDashboard.java
+++ GUI/src/main/java/views/dashboard/serviceProvider/ServiceDashboard.java
@@ -4,12 +4,19 @@ import models.utils.ArrayListBuilder;
 import views.ButtonPanelContainer;
 import views.customComponents.MyCustomJButton;
 import views.dashboard.Dashboard;
-
 import javax.swing.*;
 import java.util.List;
 
+/**
+ * This is the dashboard for the service providers users.
+ * it will initially have 3 buttons on the sideBar with the options for the
+ * services to browse through the application.
+ */
 public class ServiceDashboard extends Dashboard implements ButtonPanelContainer {
 
+    /**
+     * List of attributes, 3 buttons and 3 panels that will be alternated by the user
+     */
     private final MyCustomJButton viewUpcommingBookings;
     private final MyCustomJButton setAvailability;
     private final MyCustomJButton setStatusOfApppointment;
@@ -17,6 +24,9 @@ public class ServiceDashboard extends Dashboard implements ButtonPanelContainer 
     private SlotManagementPanel slotManagementPanel;
     private SetBookingStatusView setBookingStatusView;
 
+    /**
+     * CONSTRUCTOR INITIALISES JUST BUTTONS. Panels will initialise as user clicks on the buttons
+     */
     public ServiceDashboard() {
         viewUpcommingBookings = new MyCustomJButton("View Upcoming bookings");
         setAvailability = new MyCustomJButton("Set slots to available or not");
@@ -24,12 +34,21 @@ public class ServiceDashboard extends Dashboard implements ButtonPanelContainer 
 
     }
 
+    /**
+     * returns an array with all Dashboard buttons for the controllers to assign a listener
+     * @return
+     */
     @Override
     public List<MyCustomJButton> getButtonsPanel() {
         return new ArrayListBuilder<MyCustomJButton>().add(viewUpcommingBookings)
                 .add(setAvailability).add(setStatusOfApppointment).build();
     }
 
+    /**
+     * setters and getters
+     * @param jTable
+     * @return
+     */
     public ServiceDashboard withUpcomingBookingsPanel(JTable jTable){
         this.upComingBookingsPanel = new UpComingBookingsPanel(jTable);
         return this;
