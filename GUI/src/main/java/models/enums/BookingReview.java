87e7407df309e5c0432c81950a66699b7a7330af
felipe <marcelus20felipe@gmail.com>
2018-12-10 08:42:40 +0000

    commented BookingReview

--- GUI/src/main/java/models/enums/BookingReview.java
+++ GUI/src/main/java/models/enums/BookingReview.java
@@ -1,7 +1,12 @@
 package models.enums;
 
-import java.util.ArrayList;
-
+/**
+ * This enum represents the possible states that an BookingReview object can be assigned.
+ * A Booking review can either be with no review, with end of the world, terrible, bad,
+ * meh, ok, good, very good, superb or perfect state.
+ *
+ * Customers when adding a review, they will be prompted to choose either one of these options.
+ */
 public enum BookingReview {
 
     NO_REVIEW_ADDED,
