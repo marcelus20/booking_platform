8032d2369d2e448acb94d5adfd80f66e83af1cc0
felipe <marcelus20felipe@gmail.com>
2018-12-10 16:31:17 +0000

    commented ButtonPanelContainer

--- GUI/src/main/java/views/ButtonPanelContainer.java
+++ GUI/src/main/java/views/ButtonPanelContainer.java
@@ -1,11 +1,17 @@
 package views;
 
 import views.customComponents.MyCustomJButton;
-
-
 import java.util.List;
 
+/**
+ * This is to be used by the view classes that holds many buttons such as dashboards.
+ */
 public interface ButtonPanelContainer {
 
+    /**
+     * this method will return the list of buttons that a GUI view Frame/panel have
+     * TO BE IMPLEMENTED
+     * @return the list of Buttons
+     */
     public List<MyCustomJButton> getButtonsPanel();
 }
