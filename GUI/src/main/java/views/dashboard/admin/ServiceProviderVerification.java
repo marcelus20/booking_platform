fb5d934b30fdcd1e85fa8b4ffc4b18f75700155a
felipe <marcelus20felipe@gmail.com>
2018-12-10 15:17:23 +0000

    commented ServiceproviderVerification view class

--- GUI/src/main/java/views/dashboard/admin/ServiceProviderVerification.java
+++ GUI/src/main/java/views/dashboard/admin/ServiceProviderVerification.java
@@ -4,23 +4,41 @@ package views.dashboard.admin;
 import views.customComponents.MyCustomJPanel;
 
 import javax.swing.*;
-import java.awt.*;
 
+/**
+ * Here is the Panel for the service provider verification view.
+ * Admins can set Service provider status in this panel and will be toggled when
+ * pressed the button on the dashboard sidebar
+ */
 public class ServiceProviderVerification extends MyCustomJPanel {
 
+    /**
+     * Jtable with service providers
+     */
     private JTable tableOfServices;
 
+    /**
+     * constructor will initilise just the superclass, not the Jtable
+     */
     public ServiceProviderVerification() {
         super("List of Service Provider pendents waiting on approval/rejection", 700, 600);
-
     }
 
+    /**
+     * controllers will set the JTable by using this method after the the data from DB has been retrieved
+     * @param tableOfServices
+     * @return
+     */
     public ServiceProviderVerification withTableOfServices(JTable tableOfServices) {
         this.tableOfServices = tableOfServices;
         validate(); repaint();
         return this;
     }
 
+    /**
+     * getter
+     * @return
+     */
     public JTable getTableOfServices() {
         return tableOfServices;
     }
