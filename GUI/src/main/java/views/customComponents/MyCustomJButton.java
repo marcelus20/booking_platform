c2208f8243f36575037e3a7a7afc51695dbf31db
felipe <marcelus20felipe@gmail.com>
2018-12-10 14:41:07 +0000

    commente MyCustomJButton

--- GUI/src/main/java/views/customComponents/MyCustomJButton.java
+++ GUI/src/main/java/views/customComponents/MyCustomJButton.java
@@ -10,18 +10,35 @@ import java.awt.Dimension;
 
 public class MyCustomJButton{
 
+    /**
+     * JButton attribute
+     */
     private final JButton button;
 
+    /**
+     * Full Contructorr
+     * @param label
+     * @param width
+     * @param height
+     */
     public MyCustomJButton(final String label, final Integer width, final Integer height) {
         button = new JButton(label);
         button.setPreferredSize(new Dimension(width, height));
     }
 
+    /**
+     * String label constructor
+     * @param label
+     */
     public MyCustomJButton(String label) {
         button = new JButton(label);
         button.setPreferredSize(new Dimension(200, 70)); // DEFAULT SIZE FOR MY BUTTONS
     }
 
+    /**
+     * gettter
+     * @return
+     */
     public JButton getButton() {
         return button;
     }
