98b558d0a2b9e3e20c75a364527984262ee2d1d0
felipe <marcelus20felipe@gmail.com>
2018-12-10 14:30:55 +0000

    commented Input panel

--- GUI/src/main/java/views/customComponents/InputPanel.java
+++ GUI/src/main/java/views/customComponents/InputPanel.java
@@ -4,12 +4,30 @@ package views.customComponents;
 import javax.swing.*;
 import java.awt.*;
 
-
+/**
+ * This class wraps an TextField along with its label in a panel. Making it
+ * Well arranged/sorted and configured.
+ *
+ * Typically used in forms, when you have, for example the label email with the input right after
+ *
+ *  _______________________________________
+ * |               |______________________|
+ * |Email:         |__some@somewhere.com _|
+ * |_______________|______________________|
+ */
 public class InputPanel extends JPanel {
 
+    /**
+     * attributes
+     */
     private final MyCustomJLabel label;
     private final MyCustomJField input;
 
+    /**
+     * Constructor - it takes a String label and a Jtextfield
+     * @param label
+     * @param input
+     */
     public InputPanel(final String label, final JTextField input) {
         this.label = new MyCustomJLabel(label);
         this.input = new MyCustomJField(input);
@@ -20,6 +38,10 @@ public class InputPanel extends JPanel {
         add(this.label.getLabel());add(this.input.getInput());
     }
 
+    /**
+     * getters
+     * @return
+     */
     public JLabel getLabel() {
         return label.getLabel();
     }
