41f17c3cf4323d67961a827612b0402b69c314e1
felipe <marcelus20felipe@gmail.com>
2018-12-10 16:00:48 +0000

    commented upcommingBookings

--- GUI/src/main/java/views/dashboard/serviceProvider/UpComingBookingsPanel.java
+++ GUI/src/main/java/views/dashboard/serviceProvider/UpComingBookingsPanel.java
@@ -1,16 +1,22 @@
 package views.dashboard.serviceProvider;
 
 import views.customComponents.MyCustomJPanel;
-
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * THIS PANEL IS FOR SERVICE TO SEE UPCOMMING BOOKINGS
+ */
 public class UpComingBookingsPanel extends MyCustomJPanel{
 
+    /**
+     * THE JTABLE CONTAINING THE UPCOMMING BOOKINGS
+     */
     private final JTable upcomingBookingsTable;
 
     public UpComingBookingsPanel(JTable jTable) {
-        super("Upcoming bookings - Click on the table row to cancel a booking", 700,700);
+        super("Upcoming bookings - Click on the table row to change status of booking", 700,700);
+
 
         upcomingBookingsTable = jTable;
         JScrollPane jScrollPane = new JScrollPane(upcomingBookingsTable);
@@ -18,6 +24,10 @@ public class UpComingBookingsPanel extends MyCustomJPanel{
         getContent().add(jScrollPane);
     }
 
+    /**
+     * getter
+     * @return
+     */
     public JTable getUpcomingBookingsTable() {
         return upcomingBookingsTable;
     }
