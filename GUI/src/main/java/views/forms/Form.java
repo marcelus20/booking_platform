418797c10ec5ef843a4161051c6fa66ffb30455d
felipe <marcelus20felipe@gmail.com>
2018-12-10 16:14:40 +0000

    COMMENTED FORM

--- GUI/src/main/java/views/forms/Form.java
+++ GUI/src/main/java/views/forms/Form.java
@@ -1,36 +1,45 @@
 package views.forms;
 
 import views.customComponents.MyCustomJFrame;
-
 import javax.swing.*;
 
-
+/**
+ * This class is the container that will hold the Customer sign in form and service provider sign in form
+ *
+ * User will be able to change to one of the forms he likes by toggling customer or service form
+ */
 public class Form extends MyCustomJFrame{
+    /**
+     * List of attributes
+     */
+    private final JMenuBar bar; // menu bar
+    private final JMenu menu; // menu
+    private final JMenuItem togleCustomerForm; // menu item to toggle customer form
+    private final JMenuItem toggleServiceForm; // menu item to toggle service form
+    private SignUp registrationForm; // The signup form that can be either customer or service provider form
 
-    private final JMenuBar bar;
-    private final JMenu menu;
-    private final JMenuItem togleCustomerForm;
-    private final JMenuItem toggleServiceForm;
-    private SignUp registrationForm;
 
-
+    /**
+     * CONSTRUCTOR
+     * @param registrationForm
+     */
     public Form(SignUp registrationForm) {
-
         super("Sign Up", 600, 900);
-
         bar = new JMenuBar();
         menu = new JMenu("Registration Forms");
         togleCustomerForm = new JMenuItem("Sign up as a Customer");
         toggleServiceForm = new JMenuItem("Sign up as a Service Provider");
         this.registrationForm = registrationForm;
         menu.add(toggleServiceForm); menu.add(togleCustomerForm); bar.add(menu); addMenuBar(bar);
-
         getFramePanel().add(registrationForm);
-
         defaultClose();
-
         validadeAndRepaint();
     }
+
+    /**
+     * GETTERS
+     * @return
+     */
 
     public JMenuItem getTogleCustomerForm() {
         return togleCustomerForm;
@@ -44,6 +53,10 @@ public class Form extends MyCustomJFrame{
         return registrationForm;
     }
 
+    /**
+     * SETTER
+     * @param newForm
+     */
     public void withRegistrationForm(SignUp newForm){
         getFramePanel().removeAll();
         registrationForm = newForm;
