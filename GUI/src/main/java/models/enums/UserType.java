d2fa23be54adf4d3bacf15a06efa74b7f7091d57
felipe <marcelus20felipe@gmail.com>
2018-12-10 08:52:02 +0000

    commented UserType

--- GUI/src/main/java/models/enums/UserType.java
+++ GUI/src/main/java/models/enums/UserType.java
@@ -1,5 +1,10 @@
 package models.enums;
 
+/**
+ * This enum is responsible to map the users instance of admin to ADMIN type, customer to CUSTOMER type and service to
+ * SERVICE_PROVIDER type.
+ * Users classes such as Admin, Customer and ServiceProvider have an attribute userType which turns out to receive a value from this ENUM.
+ */
 public enum UserType{
     ADMIN, CUSTOMER, SERVICE_PROVIDER;
 }
