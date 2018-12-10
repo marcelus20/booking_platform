6fe596506e8411aefc917204e45a5abc45541a47
felipe <marcelus20felipe@gmail.com>
2018-12-10 14:34:14 +0000

    commented MyCustomFont class

--- GUI/src/main/java/views/customComponents/MyCustomFont.java
+++ GUI/src/main/java/views/customComponents/MyCustomFont.java
@@ -8,24 +8,44 @@ import java.awt.Font;
 
 public class MyCustomFont {
 
+    /**
+     * attribute
+     */
     private final Font font;
 
+    /**
+     * First constructor: just takes Font
+     * @param font
+     */
     public MyCustomFont(final String font) {
         this.font = new Font(font, Font.PLAIN, 15);
     }
 
+    /**
+     * Second constructor: empty constructor
+     */
     public MyCustomFont() {
         this.font = new Font("Serif", Font.PLAIN, 15);
     }
 
+    /**
+     * Third constructor: takes font type and size.
+     * @param fontType
+     * @param fontSize
+     */
     public MyCustomFont(final String fontType, final Integer fontSize) {
-        this.font = new Font("Serif", Font.PLAIN, fontSize);
+        this.font = new Font(fontType, Font.PLAIN, fontSize);
     }
 
+    /**
+     * Fourth takes just fontSize and sets to serif the name
+     * @param fontSize
+     */
     public MyCustomFont(Integer fontSize) {
         this.font = new Font("Serif", Font.PLAIN, fontSize);
     }
 
+    //getter
     public Font getFont() {
         return font;
     }
