a9e7ba13c1f5e2d9252bdfc3a5049cf073c2f950
felipe <marcelus20felipe@gmail.com>
2018-12-10 10:20:20 +0000

    commented BookingSlotRepository

--- GUI/src/main/java/models/repositories/BookingSlotRepository.java
+++ GUI/src/main/java/models/repositories/BookingSlotRepository.java
@@ -13,29 +13,48 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class will interact with the booking_slots table in the database schema.
+ * This implements the interface Repository of bookingSlots (Repository<BookingSlots>)#
+ *
+ * This class may also interact with the booking table, so the Repository<Booking> object will
+ * also be used as an attribute of this class, the bRep object:
+ */
 public class BookingSlotRepository implements Repository<BookingSlots> {
 
-
+    //declaring the bookingRepository object
     private Repository<Booking> bRep;
 
+    /**
+     * Constructor
+     */
     public BookingSlotRepository(){
+        // initialising bookingRepository
         bRep = new BookingRepository();
     }
 
 
+    /**
+     * Adds a bookingSlots to DB, may be implemented later
+     * @param obj
+     * @throws SQLException
+     */
     @Override
     public void addToDB(BookingSlots obj) throws SQLException {
 
     }
 
+    /**
+     * This function selects will download the a bookingSlot from DB by finding it by the id passed.
+     * The id is the id of service provider, not considering the timestamp of the slot.
+     * @param id
+     * @return
+     * @throws SQLException
+     */
     @Override
     public BookingSlots selectObjById(Object id) throws SQLException {
-
         BookingSlots slot = new BookingSlots();
         Booking b = new Booking();
-
-
-
         ResultSet rs = Database.database().getStmt().executeQuery("SELECT * FROM booking_slots WHERE id = "+id+";");
 
         while (rs.next()){
@@ -43,14 +62,10 @@ public class BookingSlotRepository implements Repository<BookingSlots> {
             slot.withServiceId(rs.getString("s_id"));
             slot.withAvailability(rs.getBoolean("availability"));
         }
-
         if(!slot.getAvailability()){
             b = (Booking) bRep.selectObjById(id);
         }
-
         slot.withBooking(b);
-
-
         return slot;
     }
 
@@ -64,41 +79,50 @@ public class BookingSlotRepository implements Repository<BookingSlots> {
         return null;
     }
 
-
+    /**
+     * This function will update a slot by a given tuple of ids (timestamp, service id) with the status of available
+     * or not available (true or false)
+     * @param id
+     * @param status
+     * @throws SQLException
+     */
     public void updateBookingSlotAvailability(Tuple<String, String> id, Boolean status) throws SQLException {
-
         Integer i = 0;
-
         if(status){
             i = 1;
         }
-        System.out.println(i);
-
+        System.out.println(i); // preparing query
         String query = "UPDATE booking_slots SET availability = "
                 +i+" WHERE timestamp = '"+ id.get_1() + "' AND s_id = '"+id.get_2()+"' ;";
         System.out.println(query);
-
             Database.database().getStmt().executeUpdate(query);
-
     }
 
+    /**
+     * This function adds a slot to the database taking into account the id of of the service provider
+     * and the list of slots the service has made available in the GUI.
+     * @param id
+     * @param selectedSlots
+     * @throws SQLException
+     */
     public void addSlotsToDB(String id, List<MyCustomDateAndTime> selectedSlots) throws SQLException {
-
-
-        selectedSlots.forEach(selectedSlot->{
-            try {
+        selectedSlots.forEach(selectedSlot->{// for each slots in list, insert them into the database
+            try {//execute query
                 Database.database().getStmt().executeUpdate("INSERT INTO booking_slots values('"
-                        +selectedSlot.getTimestamp()+"', "+ id +", '1') ;");
+                        +selectedSlot.getTimestamp()+"', "+ id +", '1') ;"); // 1 stands for true.
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         });
-
     }
 
+
+    /**
+     * this method gets the list of the Booking slots from Db
+     */
     public List<BookingSlots> getList(String id) throws SQLException {
         List<BookingSlots> slots = new ArrayList<>();
-
+        //executing the query to retrivve the booking slots with a given id
         ResultSet rs = Database.database().getStmt().executeQuery("SELECT * FROM booking_slots WHERE s_id = '"+id+"';");
         while (rs.next()){
             BookingSlots bookingSlots= new BookingSlots();
@@ -107,10 +131,18 @@ public class BookingSlotRepository implements Repository<BookingSlots> {
             bookingSlots.withAvailability(rs.getBoolean("availability"));
             slots.add(bookingSlots);
         }
-
+        //return the list of slots
         return slots;
     }
 
+    /**
+     * This method will update a slot availability to available or not available, getting the timestamp and service ID.
+     * of that booking
+     * @param timestamp
+     * @param serviceId
+     * @param b
+     * @throws SQLException
+     */
     public void updateBookingSlotAvailability(Timestamp timestamp, String serviceId, boolean b) throws SQLException {
 
         Database.database().getStmt().executeUpdate("UPDATE booking_slots SET availability = "+b+" WHERE timestamp = " +
