d0d8c479188d666978dd0cdc327a94541cfcac0c
felipe <marcelus20felipe@gmail.com>
2018-12-10 15:31:42 +0000

    commented the ComplaintPanel

--- GUI/src/main/java/views/dashboard/customer/ComplaintPanel.java
+++ GUI/src/main/java/views/dashboard/customer/ComplaintPanel.java
@@ -3,27 +3,38 @@ package views.dashboard.customer;
 import models.enums.BookingReview;
 import views.customComponents.MyCustomJButton;
 import views.customComponents.MyCustomJPanel;
-
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Complaint view Panel, for Customers to add a review about the a booking (bookings will be listed
+ * just if it is set to COMPLETE by a service provider) and add a complaint about a service provider.
+ *
+ */
 public class ComplaintPanel extends MyCustomJPanel {
 
-    private JTable table;
-    private final MyCustomJPanel listOfBookingsPanel;
-    private final MyCustomJPanel complaintContainer;
-    private final MyCustomJButton submit;
-    private final JComboBox<BookingReview> bookingReviewComboBox;
-    private final MyCustomJPanel complaintManager;
-    private final JTextArea textArea;
-    private final MyCustomJButton sendComplaint;
+    /**
+     * List of attributes.
+     */
+    private JTable table;//to list the Bookings with the complete status.
+    private final MyCustomJPanel listOfBookingsPanel; // the panel for displaying the table
+    private final MyCustomJPanel complaintContainer;// the panel for displaying the complaint area
+    private final MyCustomJButton submit; //the button to save the review status to DB
+    private final JComboBox<BookingReview> bookingReviewComboBox; // the combo box with the review options (See Review enum)
+    private final MyCustomJPanel complaintManager;// area for writing down a complaint
+    private final JTextArea textArea; // input that will store the text about the complaint
+    private final MyCustomJButton sendComplaint; // button to save the complaint about a service provider in the database
 
+    /**
+     * Constructor:
+     * @param options
+     */
     public ComplaintPanel(BookingReview[] options) {
         super("Review/Complaints panel - Only applied for bookings with COMPLETE status", 750, 700);
         submit = new MyCustomJButton("Submit", 100,40);
         bookingReviewComboBox = new JComboBox<BookingReview>(options);
 //        bookingReviewComboBox.setPreferredSize(new Dimension(300,300));
-        listOfBookingsPanel = new MyCustomJPanel("List Of Bookings", 300, 10);
+        listOfBookingsPanel = new MyCustomJPanel("List Of Bookings with COMPLETE status", 300, 10);
         complaintContainer = new MyCustomJPanel("Review Manager", 300, 10);
         complaintManager = new MyCustomJPanel("Complaints", 250, 250);
         sendComplaint =  new MyCustomJButton("Send Complaint", 200,50);
@@ -34,11 +45,11 @@ public class ComplaintPanel extends MyCustomJPanel {
         complaintManager.getContent().add(sendComplaint.getButton());
         getContent().setLayout(new BorderLayout());
         getContent().add(listOfBookingsPanel, BorderLayout.LINE_START);
-
-
-
     }
 
+    /**
+     * this method adds the complaintContainer in the "content" panel of the super class.
+     */
     public void showComplaintContainer(){
         complaintContainer.getContent().setLayout(new GridLayout(5,1));
         complaintContainer.getContent().add(bookingReviewComboBox);
@@ -50,6 +61,10 @@ public class ComplaintPanel extends MyCustomJPanel {
     }
 
 
+    /**
+     * GETTERS
+     * @return
+     */
     public MyCustomJButton getSubmit() {
         return submit;
     }
@@ -61,6 +76,11 @@ public class ComplaintPanel extends MyCustomJPanel {
     public JTable getTable() {
         return table;
     }
+
+    /**
+     * SETTERS
+     * @param t
+     */
 
     public void withListofBookingPanel(JTable t) {
         this.table = t;
